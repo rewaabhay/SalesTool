ae888ba5c2f56ff144a5b34f217239320fa6cd35
Abhay Tiwari <atiwari@asicentral.com>
2015-01-05 17:45:59 +0530

    Initial Check in of Sales Tool.

---
+++ SalesTool/src/org/demo/salestool/Main.java
@@ -0,0 +1,16 @@
+package org.demo.salestool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data=new SalesData();
+		displayGreeting();
+		data.display();
+
+	}
+	public static void displayGreeting() {
+		System.out.println("Hello Happy Sales People!!");
+		System.out.println("This app shows sales data!!");
+	}
+
+}
---
+++ SalesTool/src/org/demo/salestool/SalesData.java
@@ -0,0 +1,11 @@
+package org.demo.salestool;
+
+public class SalesData {
+int data[]={0,4,42};
+
+public void display()
+{
+	System.out.println("TODO: implement data display");
+}
+	
+}
