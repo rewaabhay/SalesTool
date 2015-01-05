8cc61d750afae666f39a65981a788fa09aaee261
Abhay Tiwari <atiwari@asicentral.com>
2015-01-05 18:54:40 +0530

    Fixed case of initial greeting display.

--- SalesTool/src/org/demo/salestool/Main.java
+++ SalesTool/src/org/demo/salestool/Main.java
@@ -9,8 +9,9 @@ public class Main {
 
 	}
 	public static void displayGreeting() {
-		System.out.println("Hello Happy Sales People!!");
-		System.out.println("This app shows sales data!!");
+		System.out.println("Hello Happy Sales People!!!");
+		System.out.println("This app shows sales data!!!");
+		System.out.println("----------------------------");
 		System.out.println("Test 1");
 		System.out.println("Test 2");
 	}
